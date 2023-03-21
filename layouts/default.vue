fe843c6ddad7fae1f3f08601ba4edc85a9b88c48
stevecyj <ycsteven.tsao@gmail.com>
2023-03-21 17:06:57 +0800

    feat: public, assets

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,17 @@
 <template>
   <div>
-    <nav>導航欄</nav>
+    <nav>
+      導航欄
+      <img class="avatar" src="/images/image_1.png" alt="" />
+      <img class="avatar" src="~assets/images/image_2.png" alt="" />
+    </nav>
     <slot></slot>
   </div>
 </template>
+<style scoped>
+.avatar {
+  width: 50px;
+  border: 1px solid rgb(218, 218, 218);
+  border-radius: 50%;
+}
+</style>
