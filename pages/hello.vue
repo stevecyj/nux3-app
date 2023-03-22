86023110dc5d4413baf2afb8c6878520dc43102f
stevecyj <ycsteven.tsao@gmail.com>
2023-03-22 17:27:13 +0800

    feat: get message from server api

---
+++ pages/hello.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    {{ message }}
+  </div>
+</template>
+
+<script setup lang="ts">
+// 請求資料 $fetch
+interface Data {
+  message: string;
+}
+const { message } = await $fetch<Data>('/api/hello');
+</script>
