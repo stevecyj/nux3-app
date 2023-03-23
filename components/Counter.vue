f8adc57e29f72a9e1999a66d138c64aa18168f7f
stevecyj <ycsteven.tsao@gmail.com>
2023-03-23 17:44:25 +0800

    WIP: useState

---
+++ components/Counter.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="p-4">
+    Counter1: {{ counter1 }} Counter2: {{ counter2 }}
+    <div class="mt-2">
+      <van-button
+        @click="
+          counter1++;
+          counter2++;
+        "
+        >+</van-button
+      >
+      <van-button
+        @click="
+          counter1--;
+          counter2--;
+        "
+        >-</van-button
+      >
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+const counter1 = useCounter();
+const counter2 = ref(1);
+</script>
--- pages/count.vue
+++ pages/count.vue
@@ -2,9 +2,11 @@
   <div class="p-4">
     Counter: {{ counter }}
     <div class="mt-2">
-      <NButton @click="counter++">+</NButton>
-      <NButton @click="counter--">-</NButton>
+      <!--      <van-button type="primary" @click="counter++">+</van-button>-->
+      <!--      <van-button type="danger" @click="counter&#45;&#45;">-</van-button>-->
     </div>
+    <Counter></Counter>
+    <Counter></Counter>
   </div>
 </template>
 
