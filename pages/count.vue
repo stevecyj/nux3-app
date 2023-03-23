e3c37c5fa63318cbc00def9ae9f3f0b0735cc4c5
stevecyj <ycsteven.tsao@gmail.com>
2023-03-23 17:01:50 +0800

    feat; useState

---
+++ pages/count.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="p-4">
+    Counter: {{ counter }}
+    <div class="mt-2">
+      <NButton @click="counter++">+</NButton>
+      <NButton @click="counter--">-</NButton>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+const counter = useState('counter', () => Math.round(Math.random() * 1000));
+</script>
