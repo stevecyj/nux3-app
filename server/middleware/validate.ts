6b8b56d6086d2a8986534ef8f9450374a0fb73d9
stevecyj <ycsteven.tsao@gmail.com>
2023-03-29 12:40:20 +0800

    feat: demo be valid

---
+++ server/middleware/validate.ts
@@ -0,0 +1,16 @@
+// export default defineEventHandler((event) => {
+//   //   擴展 context 物件
+//   event.context.userInfo = { user: 'JOJO' };
+//   //   驗證請求資料
+//   const id = parseInt(event.context!.params!.id) as number;
+//   if (!Number.isInteger(id)) {
+//     //   拋出錯誤
+//     return sendError(
+//       event,
+//       createError({
+//         statusCode: 400,
+//         statusMessage: 'ID should be an integer',
+//       }),
+//     );
+//   }
+// });
